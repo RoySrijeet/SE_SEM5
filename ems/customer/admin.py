2c9dfdd3dab07ea21187d5371655732e3a6dbf15
Sounak Saha <34378847+Sounak97@users.noreply.github.com>
2020-01-02 04:21:39 +0530

    Add files via upload

--- ems/customer/admin.py
+++ ems/customer/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from customer.models import Caterer
+from customer.models import Decorator
+from customer.models import Hall
+from customer.models import PayMod
+from customer.models import Studio
+from customer.models import EventDetails
+from customer.models import Customer
+from customer.models import Planner
 
 # Register your models here.
+admin.site.register(Caterer)
+admin.site.register(Customer)
+admin.site.register(Hall)
+admin.site.register(EventDetails)
+admin.site.register(Decorator)
+admin.site.register(PayMod)
+admin.site.register(Planner)
+admin.site.register(Studio)
+
+
--- ems/customer/views.py
+++ ems/customer/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from  django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+#from .forms import UserRegisterForm
 # Create your views here.
-def customer(request):
-    return render(request, 'customer.html', {'name': 'Srijeet'})
+from django.template import RequestContext
+#from django.core.context_processors import csrf
+from customer.forms import Customer as RegisForm
+from customer.models import Customer as Regis
+from customer.forms import EventDetails as Ed1
+from customer.models import EventDetails as Ed2
+
+#def customer(request):
+#    return render(request, 'customer.html', {'name': 'Srijeet'})
 
 # def add(request):
 #     a = int(request.POST['num1'])
 #     b = int(request.POST['num2'])
 #     sum = a + b
 #     return render(request, 'result.html', {'result': sum})
+def register(request):
+    if request.method=='POST':
+        form=RegisForm(request.POST)
+        if form.is_valid():
+            f_name = request.POST.get('f_name', '')
+            l_name = request.POST.get('l_name', '')
+            organisation = request.POST.get('organisation', '')
+            email = request.POST.get('email', '')
+            password = request.POST.get('password', '')
+            phone = request.POST.get('phone', '')
+            regis_obj=Regis(f_name =f_name ,l_name =l_name ,organisation =organisation ,email =email ,password =password ,phone = phone )
+            regis_obj.save()
+            #return HttpResponseRedirect(reverse('customer:register'))
+    else:
+        form=RegisForm()
+
+    return render(request, 'register.html',{'form':form,}, RequestContext(request))
 
 def index(request):
     return render(request, 'index.html')
@@ -40,4 +67,45 @@ def cultural(request):
 def customize(request):
     return render(request, 'customize.html')
 
+def submit_details(request):
+    if request.method == 'POST':
+        form = Ed1(request.POST)
+        if form.is_valid():
+            email_id = request.POST.get['email_id']
 
+            date = request.POST.get['date']
+            time = request.POST.get['time']
+            cus_id = request.POST.get['cus_id']
+            hall_id = request.POST.get['hall_id']
+            studio_id = request.POST.get['studio_id']
+
+            caterer_id = request.POST.get['caterer_id']
+
+            decor_id = request.POST.get['decor_id']
+            planner_id = request.POST.get['planner_id']
+            regis_obj = Ed2(email_id=email_id,date=date,  time=time,cus_id=cus_id,  hall_id=hall_id,studio_id = studio_id,caterer_id=caterer_id,decor_id=decor_id,planner_id=planner_id)
+            regis_obj.save()
+            # return HttpResponseRedirect(reverse('customer:register'))
+            #return HttpResponseRedirect('/success/')
+    else:
+        form = Ed1()
+
+    return render(request, 'submit_details.html', {'form': form, },RequestContext(request))
+
+  #  return render(request,'submit_details.html',RequestContext(request))
+'''
+def submit_details(request):
+    if request.method == 'POST':
+        if request.POST.get('email_id') and request.POST.get('date'):
+            post = EventDeatails()
+            post.title = request.POST.get('email_id')
+            post.content = request.POST.get('date')
+            post.save()
+
+            return render(request, 'submit_details.html', {'form': form, }, RequestContext(request))
+
+    else:
+        return render('submit_details.html', {'form': form, }, RequestContext(request))
+
+
+'''
